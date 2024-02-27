3faf29e5d19103aca4e44bed64f9a1d4e2863b93
raucry <64470206+raucry@users.noreply.github.com>
2024-02-27 23:08:23 +0200

    Update webgl-utils.js

--- webgl-utils.js
+++ webgl-utils.js
@@ -1,51 +1,51 @@
-// webgl-utils.js
-// from https://webglfundamentals.org/webgl/resources/webgl-utils.js
+// script.js
+var vertexShader = `
+    attribute vec4 a_Position;
+    void main() {
+        gl_Position = a_Position;
+    }
+`;
 
-// NOTE: This file is a utility to simplify some WebGL setup. It's not authored by me (OpenAI) and is provided as a commonly used resource in WebGL development.
+var fragmentShader = `
+    precision mediump float;
+    uniform float time;
+    uniform vec2 resolution;
 
-function createShader(gl, type, source) {
-  var shader = gl.createShader(type);
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-    return shader;
-  }
+    float iTime = 0.0;
+    #define iResolution resolution
 
-  console.log(gl.getShaderInfoLog(shader));
-  gl.deleteShader(shader);
-}
+    #define ALL_COLORS 1
 
-function createProgram(gl, vertexShader, fragmentShader) {
-  var program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (success) {
-    return program;
-  }
+    // Rest of the shader code goes here...
 
-  console.log(gl.getProgramInfoLog(program));
-  gl.deleteProgram(program);
-}
+    void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+        // Rest of the mainImage function from your shader code...
+    }
 
-function createProgramFromSources(gl, shaderSources) {
-  var shaders = shaderSources.map(function(source) {
-    var type = source.type;
-    var shader = createShader(gl, type, source.source);
-    return shader;
-  });
-  return createProgram(gl, shaders[0], shaders[1]);
-}
+    void main() {
+        iTime = time;
+        mainImage(gl_FragColor, gl_FragCoord.xy);
+    }
+`;
 
-function resizeCanvasToDisplaySize(canvas) {
-  var width = canvas.clientWidth;
-  var height = canvas.clientHeight;
-  if (canvas.width !== width || canvas.height !== height) {
-    canvas.width = width;
-    canvas.height = height;
-    return true;
-  }
-  return false;
+function main() {
+    var canvas = document.getElementById('canvas');
+    var gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        console.log('WebGL not supported, falling back on experimental-webgl');
+        gl = canvas.getContext('experimental-webgl');
+    }
+
+    if (!gl) {
+        alert('Your browser does not support WebGL');
+        return;
+    }
+
+    // Inline the contents of webgl-utils.js here
+
+    // Define the createShader, createProgram, createProgramFromSources, and resizeCanvasToDisplaySize functions here
+    
+    // Call the main function here
+    main();
 }
